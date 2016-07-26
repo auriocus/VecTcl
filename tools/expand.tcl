b4c54c5df48f75ad14c76718bad3514ab494769f
Christian daheim <auriocus@gmx.de>
2016-07-26 09:51:31 +0200

    Add Tcl-based preprocessor

---
+++ tools/expand.tcl
@@ -0,0 +1,28 @@
+# create the parser from the grammar using pt
+# if it was changed.
+
+set toolpath [file dirname [info script]]
+
+package require textutil::expander
+package require fileutil
+
+if {[llength $argv]!=2} {
+	puts stderr "Usage: $argv0 input.tcl.c output.c"
+	exit -1
+}
+
+lassign $argv templatefn resultfn
+
+set template [fileutil::cat $templatefn]
+set templatepath [file dirname $templatefn]
+
+set expander [textutil::expander %AUTO%]
+$expander setbrackets "\${" "\$}"
+
+proc C {text} {
+	$::expander cappend [$::expander expand $text]
+}
+
+source [file join $templatepath defs.tcl]
+
+fileutil::writeFile $resultfn [$expander expand $template]
